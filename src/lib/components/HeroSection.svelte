9418d5287fe80c0872c74fe6adb480026a9fa7dc
Gihan_Sachith <gihansachith92@gmail.com>
2025-02-21 23:10:40 +0530

    assets

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,7 +1,14 @@
+<script>
+    import bookCover from "$assets/book_cover.png";
+    import phoneCover from "$assets/phone_cover.png";
+</script>
+
 <section class="hero">
     <div class="hero-showcase text-center bold">
         <h1 class="purple">MOVE TO</h1>
         <h1 class="purple mb-xs">SPAIN</h1>
         <h4 class="italic semi-bold">There's no need to make the same mistakes I made.</h4>
+        <img class="book-cover" src={bookCover} alt="">
+        <img class="phone-cover" src={phoneCover} alt="">
     </div>
 </section>
