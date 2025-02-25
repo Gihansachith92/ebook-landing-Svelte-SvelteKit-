de9921da5a37886e78dac90e4e17b8e28fccb839
Gihan_Sachith <gihansachith92@gmail.com>
2025-02-25 19:52:25 +0530

    faq section html

---
+++ src/lib/components/FaqItem.svelte
@@ -0,0 +1,17 @@
+<script>
+  import chevronDown from "$assets/icons/chevron-down.svg";
+</script>
+
+<button class="container">
+	<div class="question-and-answer">
+		<p class="question mb-s">what usn hshh snsjsjsm?</p>
+		<p class="answer">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. At architecto blanditiis corporis
+			dicta deleniti accusamus, corrupti fugit! Tenetur corporis autem laboriosam rem. Veniam
+			repellendus incidunt voluptatem laborum suscipit pariatur excepturi fugit cum, necessitatibus
+			odit nisi asperiores sapiente ex ratione molestiae officia quaerat perferendis voluptate eos
+			deleniti nostrum ipsa rem similique!
+		</p>
+	</div>
+  <img src={chevronDown} alt="">
+</button>
---
+++ src/lib/components/FaqSection.svelte
@@ -0,0 +1,20 @@
+<script>
+
+	import {FaqItem,Button} from "$components";
+  import twitterIcon from "$assets/icons/twitter.webp";
+
+</script>
+
+<section class="faq-section">
+  <h2 class="mb-l">Frequently asked questions</h2>
+  <div class="faq-container">
+    <FaqItem/>
+  </div>
+  <div class="additional-info">
+    <Button class="mt-s">BUY NOW</Button>
+    <p class="mt-m">Any other questions?</p>
+    <a href="https://x.com/gihan_sachith">
+      <p>Hit me up on twitter <img src={twitterIcon} class="twitter-icon" alt=""> </p>
+    </a>
+  </div>
+</section>
