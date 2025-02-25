08538c7dd42c83b079e51c78b1873064179f172e
Gihan_Sachith <gihansachith92@gmail.com>
2025-02-25 17:33:48 +0530

    authsection style

--- src/lib/components/AuthorSection.svelte
+++ src/lib/components/AuthorSection.svelte
@@ -26,3 +26,27 @@
     <img src={authorPic} alt="author img">
   </div>
 </section>
+
+<style>
+  .author-section{
+    padding: 80px 0;
+    margin-left: 12vw;
+    margin-right: 20vw;
+    max-width: 1150px;
+  }
+
+  .author-container{
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .author-container img{
+    width: 40%;
+    border: 6px solid black;
+    box-shadow: 0 4px 24px rgba(0,0,0,0.5);
+  }
+
+  .author-text{
+    width: 55%;
+  }
+</style>
